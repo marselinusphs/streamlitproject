cd21132d1d44ff387eddf7c47cdd727e6450c335
unknown <marselinusphs@gmail.com>
2023-05-31 20:27:56 +0800

    initialize

---
+++ deploying_machine_learning_model_using_streamlit.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+import pickle
+from sklearn.metrics import accuracy_score
+
+# loading the diabetes dataset to a pandas DataFrame
+diabetes_dataset = pd.read_csv('./diabetes.csv')
+
+# separating the data and labels
+X = diabetes_dataset.drop(columns='Outcome', axis=1)
+Y = diabetes_dataset['Outcome']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
+
+classifier = svm.SVC(kernel='linear')
+classifier.fit(X_train, Y_train)
+
+X_train_prediction = classifier.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+
+print('Accuracy score of the training data : ', training_data_accuracy)
+
+# accuracy score on the test data
+X_test_prediction = classifier.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+
+print('Accuracy score of the test data : ', test_data_accuracy)
+
+filename = 'trained_model.sav'
+pickle.dump(classifier, open(filename, 'wb'))
