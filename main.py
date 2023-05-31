3815b24d3bc3127e44e2223e60b12e8353b85201
unknown <marselinusphs@gmail.com>
2023-05-31 21:28:33 +0800

    initialize

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ def diabetes_prediction(input_data):
 
     # reshape the array as we are predicting for one instance
     input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-    prediction = loaded_model.predict(input_data_reshaped)
 
-    return prediction[0]
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if prediction[0] == 0:
+        return 'The person is not diabetic'
+    else:
+        return 'The person is diabetic'
+
 
 if __name__ == '__main__':
     # loading the saved model
@@ -31,13 +37,15 @@ if __name__ == '__main__':
     diabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
     age = st.text_input('Age of the Person')
 
+    # code for Prediction
+    diagnosis = ''
+
     # creating a button for Prediction
+
     if st.button('Diabetes Test Result'):
         diagnosis = diabetes_prediction(
             [pregnancies, glucose, bloodPressure, skinThickness, insulin, bmi, diabetesPedigreeFunction, age]
         )
 
-    if diagnosis == 0:
-        st.success('The person is not diabetic')
-    else:
-        st.error("The person is diabetic")
+    st.success(diagnosis)
+    st.error(diagnosis)
