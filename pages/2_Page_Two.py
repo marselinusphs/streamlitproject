6a270e924191f1ea9e7e544a707b0356d93c5adb
unknown <marselinusphs@gmail.com>
2023-06-05 00:34:25 +0800

    initialize

--- pages/2_Page_Two.py
+++ pages/2_Page_Two.py
@@ -28,13 +28,13 @@ def predict(image):
     label = label[0][0]
     return label
 
+if __name__ == '__main__':
+    st.title("Upload + Classification Example")
 
-st.title("Upload + Classification Example")
-
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("Classifying...")
-    label = predict(uploaded_file)
-    st.write('%s (%.2f%%)' % (label[1], label[2]*100))
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.write("Classifying...")
+        label = predict(uploaded_file)
+        st.write('%s (%.2f%%)' % (label[1], label[2]*100))
