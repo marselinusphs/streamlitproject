0d3e9512bb18b87408367cbc26eb02dbfa1b68ae
unknown <marselinusphs@gmail.com>
2023-06-05 14:06:04 +0800

    initialize

--- create_model_chatbot.py
+++ create_model_chatbot.py
@@ -1,14 +1,14 @@
 import nltk
-nltk.download('punkt')
-nltk.download('wordnet')
-from nltk.stem import WordNetLemmatizer
 import json
 import pickle
 import numpy as np
+import random
+from nltk.stem import WordNetLemmatizer
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
-import random
+nltk.download('punkt')
+nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
 words = []
